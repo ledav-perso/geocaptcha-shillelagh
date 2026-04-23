6018e4b50737d26a8394abb84691435f6d8965e1
ledav - perso <dcaudre@gmail.com>
2026-04-23 19:56:35 +0200

    récupération paramètres API enregistrés sur superset

--- src/geocaptcha_shillelagh/adapter.py
+++ src/geocaptcha_shillelagh/adapter.py
@@ -17,11 +17,13 @@ from shillelagh.lib import get_session
 from shillelagh.typing import RequestedOrder, Row
 
 _logger = logging.getLogger(__name__)
+_logger.setLevel(logging.DEBUG)
 
 _GC_PREFIX = "geocaptcha://"
 _GC_SESSION = "session"
 _GC_CUSER = "cuser"
 _GC_KINGPIN = "kingpin"
+
 _P = re.compile('^geocaptcha://(session|cuser|kingpin)$')
 
 
@@ -42,34 +44,34 @@ class GeocaptchaAdapter(Adapter):
         return uri.startswith(_GC_PREFIX)
 
     @staticmethod
-    def parse_uri(uri: str) -> Tuple[Optional[str]]:
+    def parse_uri(uri: str) -> tuple[str]:
         _logger.info('gc / parse_uri')
-        table = _P.match(uri)
-        if table is not None:
-            return table.groups()[0]
+        _logger.debug(f'uri: {uri}')
+        matched = _P.match(uri)
+        if matched is not None:
+            collection = matched.groups()[0]
+            _logger.debug(f'collection found: {collection}')
+            return (collection,)
         else:
-            return None
+            _logger.error('error parsing uri, no collection identified')
+            return (_GC_SESSION,)
 
-    def __init__(self):
+    def __init__(self, collection: str, base_url: str, app_id: str, api_key: str):
         _logger.info('gc / __init__')
         super().__init__()
+
+        self.collection = collection
+        self.base_url = base_url
+        self.app_id = app_id
+        self.api_key = api_key
 
     def get_data(  # pylint: disable=too-many-locals
         self,
         bounds: dict[str, Filter],
         order: list[tuple[str, RequestedOrder]],
+        limit: Optional[int] = None,
         **kwargs: Any,
     ) -> Iterator[Row]:
         _logger.info('gc / get_data')
         conn = _get_headers(kwargs)
         yeld({id: 1, info: "réussite"})
-
-    @staticmethod
-    def _get_headers(**kwargs: Any) -> Connexion:
-        logger.info('gc / _get_headers')
-        request_headers = kwargs.get("request_headers", {})
-        base_url = request_headers.base_url | None
-        app_id = request_headers.app_id | None
-        api_key = request_headers.api_key | None
-
-        return Connexion(base_url, app_id, api_key)
