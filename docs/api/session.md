12abe9ce7d0e33938b2985ecccd76a652fcf5717
ledav - perso <dcaudre@gmail.com>
2026-05-03 10:38:50 +0200

    nettoyage de l'adapter sessions

---
+++ docs/api/session.md
@@ -0,0 +1,37 @@
+# API Geocaptcha : collections
+
+```json
+{
+  "sessions": [
+    {
+      "captcha": {
+        "type": "kingpin",
+        "challenge": {
+          "name": "25757-88000",
+          "backendId": "25757-88000-backend.png",
+          "frontendId": "25757-88000-frontend.png",
+          "angle": 250
+        }
+      },
+      "_id": "6998b38e66f0bb9f02ac4535",
+      "ip": "::ffff: 127.0.0.1",
+      "success": false,
+      "attempts": 1,
+      "available": false,
+      "begin": "2026-02-20T19: 18: 38.622Z",
+      "end": null,
+      "referer": "application-client-13e274287b298d1191008af11625c30513ddd01d.eu",
+      "visited": true,
+      "challengeId": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..sVAlAd9d6y15YEvYaID7og.o0OM5DOitCxXQqDxTy8hoNLkfxTjsriuufgMtx7MyX2CfmN6r-fS7BH5AEMBpED-IWLFnt2Ue869U_Oc05aApsJ1RcsHNLw7tvv4UyXGqMqZOz2bbHa0zLQGwBUhOY0lEl4qqqWbQdDQJDP2tQ2K3Q.N8zbdbf9RSEiCgLoiBLBp2Ho9XbQkEQESlLdgT0_X3I",
+      "response": {
+        "angle": 0,
+        "h": ""
+      },
+      "createdAt": "2026-02-20T19: 18: 38.641Z",
+      "updatedAt": "2026-02-20T19: 18: 48.075Z"
+    }
+  ],
+  "nbObjects": 100,
+  "nbTotalObjects": 48995
+}
+```
--- src/geocaptcha_shillelagh/adapter.py
+++ src/geocaptcha_shillelagh/adapter.py
@@ -1,77 +1,146 @@
-import logging
-import urllib.parse
-import re
-from collections.abc import Iterator
-from datetime import date, datetime, timedelta, timezone
-from typing import Any, Optional, Union, cast
-from dataclasses import dataclass
+"""
+GeocaptchaAdapter
+---------------
+read-only Shillelagh adapter that uses Geocaptcha API admin:
 
-import dateutil.parser
-import dateutil.tz
+  - geocaptcha://session     -> get sessions collection
+  - geocaptcha://cuser       -> get cusers collection
+  - geocaptcha://kingpin     -> get kingpins collection
+
+Assumptions:
+  * Apache superset configuration adds :
+      - base_url : API Endpoint (eg https://geocaptcha.ign.fr/api/v1/admin)
+      - app_id : admin user name
+      - api_key : admin api key
+"""
+
+import logging
+import re
+
+from collections.abc import Iterator
+from typing import Any, Optional
+
+import requests
 
 from shillelagh.adapters.base import Adapter
-from shillelagh.exceptions import ImpossibleFilterError
-from shillelagh.fields import DateTime, Float, IntBoolean, Integer, Order, String
-from shillelagh.filters import Filter, Impossible, Operator, Range
-from shillelagh.lib import get_session
+from shillelagh.exceptions import ProgrammingError
+from shillelagh.fields import Field, DateTime, Integer, String, Boolean
 from shillelagh.typing import RequestedOrder, Row
+from shillelagh.filters import Filter
 
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.DEBUG)
 
 _GC_PREFIX = "geocaptcha://"
-_GC_SESSION = "session"
-_GC_CUSER = "cuser"
-_GC_KINGPIN = "kingpin"
 
-_P = re.compile('^geocaptcha://(session|cuser|kingpin)$')
+_P = re.compile("^geocaptcha://(session|cuser|kingpin)$")
 
+_GC_SESSION = {
+    "collection": "session",
+    "fields": {
+        # flatten captcha dict
+        # "captcha_type": String(),  # "kingpin",
+        # "captcha_name": String(),  # "25757-88000",
+        # "captcha_angle": Integer(),  # 250,
+        "_id": String(),  # "6998b38e66f0bb9f02ac4535",
+        "ip": String(),  # "::ffff: 127.0.0.1",
+        "success": Boolean(),  # false,
+        "attempts": Integer(),  # 1,
+        "available": Boolean(),  # false,
+        "begin": DateTime(),  # "2026-02-20T19: 18: 38.622Z",
+        "end": DateTime(),  # null,
+        "referer": String(),  # "application-client-13e274287b298d1191008af11625c30513ddd01d.eu",
+        "visited": Boolean(),  # true,
+        "response_angle": Integer(),  # 0,
+        "response_h": String(),  # "",
+        "createdAt": DateTime(),  # "2026-02-20T19: 18: 38.641Z",
+        "updatedAt": DateTime(),  # "2026-02-20T19: 18: 48.075Z"
+    },
+    "path": "sessions",
+}
 
-@dataclass
-class Connexion:
-    base_url: str
-    app_id: str
-    api_key: str
+# ---------------------------
+# Adapter
+# ---------------------------
 
 
 class GeocaptchaAdapter(Adapter):
+    """SHillelagh Adapter customized for Geocaptcha"""
+
+    supports_requested_columns = True
     safe = True
 
     @staticmethod
     def supports(uri: str, fast: bool, **kwargs: Any) -> bool:
-        _logger.info('gc / supports')
-        _logger.debug(f'uri: {uri}')
+        """is url managed by this Adapter ?"""
+        _logger.info("def supports")
+        _logger.debug("uri: %s", uri)
         return uri.startswith(_GC_PREFIX)
 
     @staticmethod
-    def parse_uri(uri: str) -> tuple[str]:
-        _logger.info('gc / parse_uri')
-        _logger.debug(f'uri: {uri}')
+    def parse_uri(uri: str) -> tuple[Optional[str]]:
+        """extract collection from uri"""
+        _logger.info("def parse_uri")
+        _logger.debug("uri: %s", uri)
         matched = _P.match(uri)
         if matched is not None:
             collection = matched.groups()[0]
-            _logger.debug(f'collection found: {collection}')
-            return (collection,)
-        else:
-            _logger.error('error parsing uri, no collection identified')
-            return (_GC_SESSION,)
+            _logger.debug("collection found: %s", collection)
+            return collection
+
+        _logger.error("error parsing uri, no collection identified")
+        return None
 
     def __init__(self, collection: str, base_url: str, app_id: str, api_key: str):
-        _logger.info('gc / __init__')
+        _logger.info("def GeocaptchaAdapter __init__")
         super().__init__()
 
-        self.collection = collection
+        match collection:
+            case "session":
+                self.path = _GC_SESSION["path"]
+                self.fields = _GC_SESSION["fields"]
+            case _:
+                _logger.error("collection unavailable: %s", collection)
+                self.path = None
+                self.fields = None
+
         self.base_url = base_url
         self.app_id = app_id
         self.api_key = api_key
 
-    def get_data(  # pylint: disable=too-many-locals
+    def get_columns(self) -> dict[str, Field]:
+        """columns return for current collection"""
+        _logger.info("def get_columns")
+        if self.fields is None:
+            return {}
+
+        return self.fields
+
+    def get_data(
         self,
         bounds: dict[str, Filter],
         order: list[tuple[str, RequestedOrder]],
-        limit: Optional[int] = None,
+        requested_columns: Optional[set[str]] = None,
         **kwargs: Any,
     ) -> Iterator[Row]:
-        _logger.info('gc / get_data')
-        conn = _get_headers(kwargs)
-        yeld({id: 1, info: "réussite"})
+        """return rows requested"""
+        _logger.info("def get_data")
+        _logger.debug(
+            "bounds: %s, requested_columns: %s, kwargs: %s",
+            bounds,
+            requested_columns,
+            kwargs,
+        )
+        headers = {"x-app-id": self.app_id, "x-api-key": self.api_key}
+        response = requests.get(f"{self.base_url}/{self.collection}", params=headers)
+        payload = response.json()
+        if not response.ok:
+            raise ProgrammingError(f'Error: {payload["error"]["message"]}')
+
+        outline = {}
+        for i, row in enumerate(jsonpath.findall(self.path, payload)):
+            outline["rowid"] = i
+            for key in self.fields.keys():
+                outline[key] = row[key] | None
+
+            yield outline
